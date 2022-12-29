0298d4e33810a4dbf7257c3425f44b5e7bc4389e
Hardik Khandelwal <khandelwal.hardik14@gmail.com>
2022-12-29 14:10:11 +0530

    Initial commit with App bar component

---
+++ app/src/main/java/com/example/videoplayercompose/components/HomeAppBar.kt
@@ -0,0 +1,47 @@
+package com.example.videoplayercompose.components
+
+import androidx.compose.foundation.background
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeAppBar(title: String,
+               openSearch: ()-> Unit,
+               openFilters: ()-> Unit){
+    SmallTopAppBar(title = {
+        Text(text = title, color = Color.White)
+    },
+    actions = {
+        IconButton(onClick = { openSearch }) {
+            Icon(imageVector = Icons.Filled.Search,
+                contentDescription = "Search",
+            tint = Color.White)
+        }
+
+        IconButton(onClick = { openFilters }) {
+            Icon(imageVector = Icons.Filled.List,
+                contentDescription = "Filter",
+                tint = Color.White)
+        }
+    },
+    colors = TopAppBarDefaults.smallTopAppBarColors(
+        containerColor = Color(0xFFF50057)
+    )
+        )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeAppBarPreview() {
+    HomeAppBar(
+        title = "EpicWorld",
+        openSearch = {},
+        openFilters = {}
+    )
+}
